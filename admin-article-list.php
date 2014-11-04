d27fecf9cfa408c26b48a7c0811b38746eac2c92
Vincent <b.vincent.iim@gmail.com>
2014-11-04 23:59:06 +0100

    final commit

---
+++ admin-article-list.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Thibaud BARDIN (https://github.com/Irvyne).
+ * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
+ */
+
+require __DIR__.'/_header-admin.php';
+
+$articles = getArticles($link);
+
+require __DIR__.'/_footer.php';
+
+
+echo $twig->render('admin-article-list.html.twig', [
+    'articles' => $articles,
+]);
--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Vincent
- * Date: 21/10/2014
- * Time: 17:45
+ * @author Thibaud BARDIN (https://github.com/Irvyne).
+ * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
  */
 
+require __DIR__.'/_header.php';
 
-require __DIR__.'/vendor/autoload.php';
+$perPage = 6; // nbArticleParPage
+$nbArticles = countArticles($link); //nbArticleTotal
+$currentPage = !empty($_GET['p']) ? (int)$_GET['p'] : 1;// numéro de la page
+$nbPages = ceil($nbArticles/$perPage); // nombre de pagination
 
 
+if (0 >= $currentPage) {
+    header('Location: index.php?p=1');
+}
+if ($currentPage > $nbPages) {
+    header('Location: index.php?p='.$nbPages);
+}
+
+$articles = getArticles($link, null, ($currentPage-1)*$perPage, $perPage);
+
+include __DIR__.'/template/articles.php';
+
+require __DIR__.'/_footer.php';
